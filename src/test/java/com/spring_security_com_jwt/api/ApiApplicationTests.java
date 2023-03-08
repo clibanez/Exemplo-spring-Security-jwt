7b34c18d4464a9807e24003513e94992e43402e4
Clibanez <clibanezzcaldas@gmail.com>
2023-03-07 22:28:02 -0300

    Projeto inicial

---
+++ src/test/java/com/spring_security_com_jwt/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security_com_jwt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
